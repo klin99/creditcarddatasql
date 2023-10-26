e0917d270c3dfa3e308196f5620e907c8e12a6d9
klin99 <148837417+klin99@users.noreply.github.com>
2023-10-25 20:47:59 -0700

    Update sqlcreditcard.sql

--- sqlcreditcard.sql
+++ sqlcreditcard.sql
@@ -18,11 +18,6 @@ AND Marital_Status = 'Single';
 ALTER TABLE [dbo].[creditcarddata]
 ADD Yearsonbook as CAST(Months_on_book / 12.0 as DECIMAL(10, 2));
 
--- What is the average amount of dependents the graduate clients have?
-SELECT AVG(CAST(Dependent_count AS INT)) AS Average_Dependents
-From [dbo].[creditcarddata]
-Where Education_Level = 'Graduate'
-
 -- Do clients with higher credit limit have higher total average transaction count? 
 Select
 Case When Credit_Limit > 10000 then 'high credit limit'
@@ -35,13 +30,6 @@ Case When Credit_Limit > 10000 then 'high credit limit'
 Else 'low credit limit'
 End;
 
--- What is the average age of the selected clients?
-SELECT AVG(customer_age) AS Average_Age
-FROM [dbo].[creditcarddata]
-WHERE Attrition_Flag = 'Existing Customer'
-AND Education_Level IN ('Graduate', 'Post-Graduate')
-AND Marital_Status = 'Single';
-
 -- What is the difference of average total transaction amount between graduate and post graduate level clients?
 SELECT AVG(Total_Trans_Amt) AS AVG_Trans_Amt_Graduate
 FROM [dbo].[creditcarddata]
@@ -51,6 +39,42 @@ SELECT AVG(Total_Trans_Amt) AS AVG_Trans_Amt_PostGraduate
 FROM [dbo].[creditcarddata]
 WHERE Education_Level = 'Post-Graduate';
 
+
 SELECT 
     (SELECT AVG(Total_Trans_Amt) FROM [dbo].[creditcarddata] WHERE Education_Level = 'Graduate') -
     (SELECT AVG(Total_Trans_Amt) FROM [dbo].[creditcarddata] WHERE Education_Level = 'Post-Graduate') AS Difference_AVG_Transaction_Amount;
+
+-- What is the average credit limit for each card category (e.g., Blue, Silver, Gold, Platinum) among existing customers?
+SELECT Card_Category, AVG(Credit_Limit) AS Avg_Credit_Limit
+FROM [dbo].[creditcarddata]
+WHERE Attrition_Flag = 'Existing Customer'
+GROUP BY Card_Category;
+
+-- Do customers with higher total relationship counts tend to have higher credit limits?
+SELECT Total_Relationship_Count, AVG(Credit_Limit) AS Avg_Credit_Limit
+FROM [dbo].[creditcarddata]
+WHERE Attrition_Flag = 'Existing Customer'
+GROUP BY Total_Relationship_Count;
+
+-- Is there a correlation between the average total transaction amount and the average total transaction count?
+SELECT AVG(Total_Trans_Amt) AS Avg_Transaction_Amount, AVG(Total_Trans_Ct) AS Avg_Transaction_Count
+FROM [dbo].[creditcarddata]
+GROUP BY Attrition_Flag;
+-- (You can perform a correlation analysis if you want to find the correlation coefficient)
+
+-- What is the distribution of average utilization ratios among customers with different education levels and marital statuses?**
+SELECT Education_Level, Marital_Status, AVG(Avg_Utilization_Ratio) AS Avg_Utilization_Ratio
+FROM [dbo].[creditcarddata]
+GROUP BY Education_Level, Marital_Status;
+
+-- What is the average amount of dependents the graduate clients have?
+SELECT AVG(CAST(Dependent_count AS INT)) AS Average_Dependents
+From [dbo].[creditcarddata]
+Where Education_Level = 'Graduate'
+
+-- What is the average age of the selected clients?
+SELECT AVG(customer_age) AS Average_Age
+FROM [dbo].[creditcarddata]
+WHERE Attrition_Flag = 'Existing Customer'
+AND Education_Level IN ('Graduate', 'Post-Graduate')
+AND Marital_Status = 'Single';
